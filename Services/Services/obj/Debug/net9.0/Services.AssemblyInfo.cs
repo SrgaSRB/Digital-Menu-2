dc558d5e077cde4757c12ccdb03cb0e00161e727
SRDJAN <srdjandelic02@gmail.com>
2025-04-29 19:29:43 +0200

    test docker

--- Services/Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee36703b46964b568d66d5880137b0b9fa29ddbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c891325b42bb07c3e43bfc1cfbeb3a66e556baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
